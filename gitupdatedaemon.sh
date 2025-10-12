082902e5f2f37dde6da8c523b007aaba2ab3f106
Andrey Kuzin <kuzinandrey@yandex.ru>
2025-10-12 09:08:17 +0300

    Refactor code and add some checks

--- gitupdatedaemon.sh
+++ gitupdatedaemon.sh
@@ -5,39 +5,63 @@
 # You can set delay by first argv (example for 1 min):
 #    ./gitupdatedaemon.sh 60
 
+usage() {
+	echo "Use: $0 <sleep> <file>"
+}
+
+[ -n "$1" -a -f "$1" ] && echo "You pass file name as delay ($1)" && usage && exit 1
+[ ! -z "$(echo -n "$1" | tr -d [0-9])" ] && echo "Sleep delay must be an integer value" && exit 1
+
 TS=$(date +%s)
 SLEEP=${1:-900}
-(cat > /dev/shm/gitdaemon-$TS.sh) << \EOF
+[ -n "$1" ] && shift
+SCR="/dev/shm/gitdaemon-$TS.sh"
+(cat > $SCR) << \EOF
 #!/bin/sh
 TS=$(date +%s)
 CWD=$(pwd)
 SLEEP=${1:-900}
 export LC_NUMERIC=C
-while [ 1 ]; do
-	cd $CWD && find . -type f -name "*.git.info" -mtime +1 -print | shuf > /dev/shm/.gitqueue-$TS;
-	[ -r /dev/shm/.gitqueue-$TS ] && [ -s /dev/shm/.gitqueue-$TS ] && \
+
+fail_work() {
+	echo "Can't create work file"
+	exit 1
+}
+
+FILE=/dev/shm/.gitqueue-$TS
+if [ -z "$2" ]; then
+	find $CWD -type f -name "*.git.info" -mtime +1 -print | shuf > $FILE || fail_work
+elif [ -r "$2" ]; then
+	cat "$2" | shuf > $FILE || fail_work
+else
+	fail_work
+fi
+
+#while [ 1 ]; do
+#	cd $CWD
+	[ -r $FILE ] && [ -s $FILE ] && \
 	while [ 1 ]; do
 		cd $CWD
-		[ ! -r /dev/shm/.gitqueue-$TS ] && break
-		[ $(cat /dev/shm/.gitqueue-$TS | wc -l) -eq 0 ] && break
-		F=$(head -n 1 /dev/shm/.gitqueue-$TS && sed -i -e "1d" /dev/shm/.gitqueue-$TS)
+		[ ! -r $FILE ] && break
+		[ $(cat $FILE | wc -l) -eq 0 ] && break
+		F=$(head -n 1 $FILE && sed -i -e "1d" $FILE)
 		[ -z "$F" ] && break
-		printf "%s" "$(date +"%F %T") - $F " >> /dev/shm/.gitqueue-$TS.log
+		printf "%s" "$(date +"%F %T") - $F " >> $FILE.log
 		ST=$(date +"%s.%N")
 		OLD_SIZE=$(egrep "^ORIGSIZE=" $F | cut -f2 -d=)
 		gitupdate.sh $F -gc
 		ET=$(date +"%s.%N")
 		NEW_SIZE=$(egrep "^ORIGSIZE=" $F | cut -f2 -d=)
-		printf "(done in %.03f sec," $(echo "$ET - $ST" | bc) >> /dev/shm/.gitqueue-$TS.log
-		printf " size old=%d" $OLD_SIZE >> /dev/shm/.gitqueue-$TS.log
-		printf " new=%d" $NEW_SIZE >> /dev/shm/.gitqueue-$TS.log
-		printf " delta=%d)" $(echo "$NEW_SIZE - $OLD_SIZE" | bc) >> /dev/shm/.gitqueue-$TS.log
-		echo >> /dev/shm/.gitqueue-$TS.log
+		printf "(done in %.03f sec," $(echo "$ET - $ST" | bc) >> $FILE.log
+		printf " size old=%d" $OLD_SIZE >> $FILE.log
+		printf " new=%d" $NEW_SIZE >> $FILE.log
+		printf " delta=%d)" $(echo "$NEW_SIZE - $OLD_SIZE" | bc) >> $FILE.log
+		echo >> $FILE.log
 		sleep $SLEEP
 	done
-	sleep $SLEEP
-done
+#	sleep $SLEEP
+#done
 EOF
-chmod a+x /dev/shm/gitdaemon-$TS.sh
-nohup /dev/shm/gitdaemon-$TS.sh $SLEEP 1> /dev/null 2> /dev/null &
-sleep 1 && rm /dev/shm/gitdaemon-$TS.sh
+chmod a+x $SCR
+nohup $SCR $SLEEP $@ 1> /dev/null 2> /dev/null &
+sleep 1 && rm $SCR
